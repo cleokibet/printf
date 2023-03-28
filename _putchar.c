dfa2c9daf006a096f9bc91b234809c987dded8da
yvechemtai <123057065+yvechemtai@users.noreply.github.com>
2023-03-28 19:26:47 +0300

    Updated the return of _putchar.c function

--- _putchar.c
+++ _putchar.c
@@ -4,8 +4,8 @@
  * _putchar - writes the character c to stdout
  * @c: The character to print
  *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * Return: The character written as an unsigned char.
+ * On error, it returns EOF.
  */
 
 
